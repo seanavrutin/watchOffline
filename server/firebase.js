5297610272492c9c6f10f686ad9a1aa1c68f68ed
Sean Avrutin <Sean.Avrutin@amdocs.com>
2026-03-22 19:22:57 +0200

    refactor Firebase service account initialization to support JSON input or file path
    Updated the Firebase initialization logic to allow for service account credentials to be provided as a JSON string via an environment variable, falling back to the previous file path method if not provided.

--- server/firebase.js
+++ server/firebase.js
@@ -1,9 +1,13 @@
 const admin = require('firebase-admin');
 const { getFirestore } = require('firebase-admin/firestore');
-const path = require('path');
 
-const serviceAccountPath = path.resolve(__dirname, process.env.FIREBASE_SERVICE_ACCOUNT_PATH);
-const serviceAccount = require(serviceAccountPath);
+let serviceAccount;
+if (process.env.FIREBASE_SERVICE_ACCOUNT_JSON) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+} else {
+  const path = require('path');
+  serviceAccount = require(path.resolve(__dirname, process.env.FIREBASE_SERVICE_ACCOUNT_PATH));
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
